1c5a10eba045c22b28162e80e506290b7dbb73ce
Kyrolos Magdy <pero.kero01@gmail.com>
2019-01-03 17:57:36 +0200

    Updates

---
+++ precache-manifest.a62b6d951035e25be9dd566ab282ae4e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed5b866338b2c26ec8161f3fc05d874c",
+    "url": "/Myportofolio/./static/media/JeopardyApp.ed5b8663.png"
+  },
+  {
+    "revision": "24e33e04fe7d7d9136e0",
+    "url": "/Myportofolio/./static/css/main.5776d742.chunk.css"
+  },
+  {
+    "revision": "90065d5a301d3d60f36a808b0a93a332",
+    "url": "/Myportofolio/./static/media/SkiResortGem.90065d5a.png"
+  },
+  {
+    "revision": "df76170f105c2640d85d",
+    "url": "/Myportofolio/./static/js/1.df76170f.chunk.js"
+  },
+  {
+    "revision": "f15c79f27adca83ceb76",
+    "url": "/Myportofolio/./static/js/runtime~main.f15c79f2.js"
+  },
+  {
+    "revision": "f2ff1d648dbcf79e68e86800df0deaf1",
+    "url": "/Myportofolio/./static/media/myphoto.f2ff1d64.jpg"
+  },
+  {
+    "revision": "24e33e04fe7d7d9136e0",
+    "url": "/Myportofolio/./static/js/main.24e33e04.chunk.js"
+  },
+  {
+    "revision": "74a6361691932316c2eb898ec0d41215",
+    "url": "/Myportofolio/./static/media/RopoFriend.74a63616.PNG"
+  },
+  {
+    "revision": "7f20f6778759afd04fc3c5a9a17b320d",
+    "url": "/Myportofolio/./static/media/LetsGetCookin.7f20f677.png"
+  },
+  {
+    "revision": "8f52593a6884dff83dc7486828814fd2",
+    "url": "/Myportofolio/./static/media/Adios.8f52593a.png"
+  },
+  {
+    "revision": "df76170f105c2640d85d",
+    "url": "/Myportofolio/./static/css/1.60e1efd9.chunk.css"
+  },
+  {
+    "revision": "8a41d8cbec506dd8cd2ffa5cd11b2f34",
+    "url": "/Myportofolio/./index.html"
+  }
+];
